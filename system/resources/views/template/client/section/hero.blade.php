06cec33ad193789e337484ff49733f42d98a4772
SyarifahTasyaAldawiyah <91304498+SyarifahTasyaAldawiyah@users.noreply.github.com>
2021-12-04 12:45:22 +0700

    Initial commit

---
+++ system/resources/views/template/client/section/hero.blade.php
@@ -0,0 +1,13 @@
+<header class="masthead" style="">
+  <div class="container position-relative px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-md-10 col-lg-8 col-xl-7">
+              <div class="site-heading">
+                  <h1>Beauty-Blog</h1>
+                  <span class="subheading">KUMPULAN ARTIKEL SEPUTAR KECANTIKAN</span>
+                  <span class="subheading">Blog By Syarifah Tasya Aldawiyah</span>
+              </div>
+          </div>
+      </div>
+  </div>
+</header>
